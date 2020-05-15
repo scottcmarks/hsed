beecba41357b113cc1eaea68768d2d8b0ea5feb6
Scott Marks <scott@magnolia-heights.com>
2020-05-14 23:30:25 -0400

    Update

--- src/Data/SizedText.hs
+++ src/Data/SizedText.hs
@@ -57,8 +57,9 @@ import           Prelude              hiding (drop, length, map, replicate,
                                        take)
 
 import           Data.Proxy
-import           Data.SizedText.Class (create, createLeft, createRight, fromNat)
-import qualified Data.SizedText.Class as C (Elem, IsSizedText (..), Sized)
+import           Data.SizedText.Class (Sized, create, createLeft, createRight,
+                                       fromNat)
+import qualified Data.SizedText.Class as C (Elem, IsSizedText (..))
 import           Data.SizedText.TH
 
 
@@ -69,53 +70,74 @@ import           Data.SizedText.TH
 -- >>> :set -XTypeApplications
 -- >>> :set -Wno-type-defaults
 -- >>> import           Data.Proxy
+-- >>> import           Data.ByteString      (ByteString)
 -- >>> import           Data.Char(toUpper)
 -- >>> import           Data.String          (IsString (..))
 
 
--- | Append two C.Sizeds together.
+-- | Append two Sizeds together.
+-- append $(sz "foo") $(st "bear")
+--   :: (C.IsSizedText a, IsString a) => Sized a 4 7
 --
 -- >>> :type append $(sz "foo") $(st "bear")
 -- append $(sz "foo") $(st "bear")
---   :: (C.IsSizedText a, IsString a) => C.Sized a 4 7
+--   :: (C.IsSizedText a, IsString a) => Sized a 4 7
 -- >>> append $(sz "foo") $(st "bear")
 -- "foobear"
 -- >>> :type append $(st "Hello, ") $(sz "world!")
 -- append $(st "Hello, ") $(sz "world!")
---   :: (C.IsSizedText a, IsString a) => C.Sized a 7 13
+--   :: (C.IsSizedText a, IsString a) => Sized a 7 13
 -- >>> append $(st "Hello, ") $(sz "world!")
 -- "Hello, world!"
 append ::
      forall a l1 u1 l2 u2. (C.IsSizedText a, KnownNat l1, KnownNat u1, KnownNat l2, KnownNat u2)
-  => C.Sized a l1 u1
-  -> C.Sized a l2 u2
-  -> C.Sized a (l1 + l2) (u1 + u2)
+  => Sized a l1 u1
+  -> Sized a l2 u2
+  -> Sized a (l1 + l2) (u1 + u2)
 append a b = C.unsafeCreate $ C.append (C.unwrap a) (C.unwrap b)
 
--- | Construct a new C.Sized of maximum length from a basic element.
+-- | Construct a new Sized of maximum length from a basic element.
 --
--- >>> replicate '=' :: C.Sized String 5 10
+-- >>> replicate '=' :: Sized String 5 10
 -- "=========="
 replicate ::
      forall a l u. (C.IsSizedText a, KnownNat l, KnownNat u)
   => C.Elem a
-  -> C.Sized a l u
+  -> Sized a l u
 replicate e = C.unsafeCreate $ C.replicate t e
   where
     t = fromNat (Proxy @u)
 
--- | Map a C.Sized to a C.Sized of the same length.
+-- | Map a Sized to a Sized of the same length.
 --
--- >>> map toUpper $(st "Hello") :: C.Sized String 5 5
+-- >>> map toUpper $(st "Hello") :: Sized String 5 5
 -- "HELLO"
 map ::
-     C.IsSizedText a => (C.Elem a -> C.Elem a) -> C.Sized a l u -> C.Sized a l u
+     C.IsSizedText a => (C.Elem a -> C.Elem a) -> Sized a l u -> Sized a l u
 map f s = C.unsafeCreate $ C.map f $ C.unwrap s
 
--- | Reduce C.Sized length, preferring elements on the left.
+-- | Reduce Sized length, preferring elements on the left.
 --
--- >>> take $(sz "Foobar") :: C.Sized String 0 3
+-- >>> take $(sz "Foobar") :: Sized String 0 3
 -- "Foo"
+-- >>> :t take $(st "Hello")
+-- take $(st "Hello")
+--   :: (C.IsSizedText a, KnownNat l2, KnownNat u2, IsString a,
+--       (l2 <=? 5) ~ 'True) =>
+--      Sized a l2 u2
+-- >>> :t take $(st "Hello") :: Sized ByteString 4 32
+-- take $(st "Hello") :: Sized ByteString 4 32
+--   :: Sized ByteString 4 32
+-- >>> take $(st "Hello") :: Sized ByteString 4 32
+-- "Hello"
+-- >>> length (take $(st "Hello") :: Sized ByteString 4 32)
+-- 5
+-- >>> take $(st "Hello") :: Sized ByteString 4 32
+-- "Hello"
+-- >>> take $(st "Hello") :: Sized ByteString 4 4
+-- "Hell"
+-- >>> take $(st "HelloHelloHelloHelloHelloHelloHello") :: Sized ByteString 4 32
+-- "HelloHelloHelloHelloHelloHelloHe"
 take ::
      forall a l1 u1 l2 u2.
      ( C.IsSizedText a
@@ -125,17 +147,17 @@ take ::
      , KnownNat u2
      , l2 <= l1
      )
-  => C.Sized a l1 u1
-  -> C.Sized a l2 u2
+  => Sized a l1 u1
+  -> Sized a l2 u2
 take s = C.unsafeCreate $ C.take t $ C.unwrap s
   where
     t = fromNat (Proxy @u2)
 
--- | Reduce C.Sized length, preferring elements on the right.
+-- | Reduce Sized length, preferring elements on the right.
 --
--- >>> drop $(st "Foobar") :: C.Sized String 2 2
+-- >>> drop $(st "Foobar") :: Sized String 2 2
 -- "ar"
--- >>> drop $(sz "Foobar") :: C.Sized String 0 2
+-- >>> drop $(sz "Foobar") :: Sized String 0 2
 -- "ar"
 drop ::
      forall a l1 u1 l2 u2.
@@ -146,8 +168,8 @@ drop ::
      , KnownNat u2
      , l2 <= l1
      )
-  => C.Sized a l1 u1
-  -> C.Sized a l2 u2
+  => Sized a l1 u1
+  -> Sized a l2 u2
 drop s = C.unsafeCreate $ C.drop (C.length s' - t) s'
   where
     s' = C.unwrap s
@@ -156,7 +178,7 @@ drop s = C.unsafeCreate $ C.drop (C.length s' - t) s'
 -- | Obtain length bounds from the type.
 bounds ::
      forall a l u. (C.IsSizedText a, KnownNat l, KnownNat u)
-  => C.Sized a l u
+  => Sized a l u
   -> (Int, Int)
 bounds _ = (lower, upper) -- FIXME: Bounds type from Ix?
   where
@@ -166,11 +188,11 @@ bounds _ = (lower, upper) -- FIXME: Bounds type from Ix?
 -- | Obtain value-level length.  Consult the actual data value.
 length ::
      forall a l u. (C.IsSizedText a, KnownNat l, KnownNat u)
-  => C.Sized a l u
+  => Sized a l u
   -> Int
 length = C.length . C.unwrap
 
--- | Fill a C.Sized with extra elements up to target length, padding
+-- | Fill a Sized with extra elements up to target length, padding
 -- original elements to the left.
 padLeft ::
      forall a l1 u1 l2 u2.
@@ -184,9 +206,9 @@ padLeft ::
      , u1 <= u2
      )
   => C.Elem a
-  -> C.Sized a l1 u1
-  -> C.Sized a l2 u2
-padLeft pad = drop . ((replicate pad :: C.Sized a 0 u2) `append`)
+  -> Sized a l1 u1
+  -> Sized a l2 u2
+padLeft pad = drop . ((replicate pad :: Sized a 0 u2) `append`)
 
 -- | Like 'padLeft', but original elements are padded to the right.
 padRight ::
@@ -201,6 +223,6 @@ padRight ::
      , u1 <= u2
      )
   => C.Elem a
-  -> C.Sized a l1 u1
-  -> C.Sized a l2 u2
-padRight pad = take . (`append` (replicate pad :: C.Sized a 0 u2))
+  -> Sized a l1 u1
+  -> Sized a l2 u2
+padRight pad = take . (`append` (replicate pad :: Sized a 0 u2))

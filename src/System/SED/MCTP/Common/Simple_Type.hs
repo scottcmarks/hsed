493be3d14b19ad19a02d46d0e1de3d804a9f6f75
Scott Marks <scott@magnolia-heights.com>
2020-07-09 22:57:03 -0400

    whitespace

--- src/System/SED/MCTP/Common/Simple_Type.hs
+++ src/System/SED/MCTP/Common/Simple_Type.hs
@@ -14,8 +14,7 @@ Core data "simple" type formats
 -}
 
 module System.SED.MCTP.Common.Simple_Type
-  (
-      Core_bytes(..)
+  (   Core_bytes(..)
   ,   Core_integer(..)
   ,   Core_max_bytes(..)
   ,   Core_uinteger(..)
@@ -39,6 +38,7 @@ module System.SED.MCTP.Common.Simple_Type
   ,   Core_password
   ,   Core_type_def_max_size
   ,   Core_type_def
+  ,   Core_max_bytes_64
 
   ,   Core_halfuid
   ,   Core_uid
@@ -85,22 +85,23 @@ type Core_name          = Core_max_bytes 32
 type Core_password      = Core_max_bytes 32
 type Core_type_def_max_size = 128
 type Core_type_def      = Core_max_bytes Core_type_def_max_size
+type Core_max_bytes_64  = Core_max_bytes 64
 
 type Core_halfuid       = Core_bytes_4
 type Core_uid           = Core_bytes_8
 
-type Core_uinteger_1    = Core_uinteger 1
+type Core_uinteger_1    = Core_uinteger   1
 type Core_uinteger_128  = Core_uinteger 128
-type Core_uinteger_2    = Core_uinteger 2
-type Core_uinteger_20   = Core_uinteger 20
-type Core_uinteger_21   = Core_uinteger 21
-type Core_uinteger_24   = Core_uinteger 24
+type Core_uinteger_2    = Core_uinteger   2
+type Core_uinteger_20   = Core_uinteger  20
+type Core_uinteger_21   = Core_uinteger  21
+type Core_uinteger_24   = Core_uinteger  24
 type Core_uinteger_256  = Core_uinteger 256
-type Core_uinteger_28   = Core_uinteger 28
-type Core_uinteger_30   = Core_uinteger 30
-type Core_uinteger_36   = Core_uinteger 36
-type Core_uinteger_4    = Core_uinteger 4
-type Core_uinteger_48   = Core_uinteger 48
-type Core_uinteger_64   = Core_uinteger 64
-type Core_uinteger_66   = Core_uinteger 66
-type Core_uinteger_8    = Core_uinteger 8
+type Core_uinteger_28   = Core_uinteger  28
+type Core_uinteger_30   = Core_uinteger  30
+type Core_uinteger_36   = Core_uinteger  36
+type Core_uinteger_4    = Core_uinteger   4
+type Core_uinteger_48   = Core_uinteger  48
+type Core_uinteger_64   = Core_uinteger  64
+type Core_uinteger_66   = Core_uinteger  66
+type Core_uinteger_8    = Core_uinteger   8

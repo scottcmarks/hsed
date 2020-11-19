1ff31bf770374bccc84ecd788ea5491da0ab290b
Scott Marks <scott@magnolia-heights.com>
2020-11-19 13:26:36 -0500

    Update

--- src/System/SED/MCTP/Common/GDPTest.hs
+++ src/System/SED/MCTP/Common/GDPTest.hs
@@ -9,32 +9,31 @@ where
 
 import           GDP
 
-import qualified Data.List as L
 import           Data.Ord
-import qualified Data.Set  as S
-import           GHC.Base  (Int, error, undefined)
+import qualified Data.Set as S
+import           GHC.Base (error)
 
--- Introduce a predicate `SortedBy comp`, indicating that
--- the value has been sorted by the comparator named `comp`.
-newtype SortedBy comp name = SortedBy Defn
-type role SortedBy nominal nominal
 
--- Sort a value using the comparator named `comp`. The
--- resulting value will satisfy `SortedBy comp`.
-sortBy :: ((a -> a -> Ordering) ~~ comp)
-       -> [a]
-       -> ([a] ?SortedBy comp)
-sortBy (The comp) xs = assert (L.sortBy comp xs)
-
--- Introduce a predicate `MemberOf set`, indicating that
--- the value has been sorted by the setarator named `set`.
+-- Introduce a predicate `MemberOf set`, indicating that the value
+-- has been validated by membership in the collection named `set`.
 newtype MemberOf set name = MemberOf Defn
 type role MemberOf nominal nominal
 
--- Sort a value using the setarator named `set`. The
+-- Validate a value using the collection named `set`. The
 -- resulting value will satisfy `MemberOf set`.
 isMemb :: Ord a =>
           (S.Set a ~~ set)
        -> a
        -> (a ?MemberOf set)
-isMemb (The set) x = assert (if S.member x set then x else error "x is not a member of set")
+isMemb (The set) x = assert (if S.member x set then x else error "x is not a member of set") -- TODO shows
+
+-- @
+-- src/System/SED/MCTP/Common/GDPTest.hs:28:1-92: warning: [-Wincomplete-patterns] …
+--     Pattern match(es) are non-exhaustive
+--     In an equation for ‘isMemb’: Patterns not matched: _ _
+-- @
+--
+-- This warning does not appear if the view pattern @the -> set@ is used instead.
+-- Odd, since that is what @The set@ expands to.
+
+isMemb _ _ = assert (error "Shouldn't happen -- x is x")  -- TODO shows
